573b87eaa53683f6784fa40630bae5b18a6c3b5c
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-16 14:53:49 -0700

    Add a variety of Mongoose examples.

--- app.js
+++ app.js
@@ -1,61 +1,130 @@
-// Adapted from https://mongodb.github.io/node-mongodb-native/3.7/quick-start/quick-start/
-
 // Module imports.
 
-const { MongoClient } = require('mongodb');
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-// MongoDB setup.
+// Global variables.
 
 const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
 const dbName = 'fruitsDB';
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  findDocuments(db, function() {
-    client.close();
-  });
+
+// Mongoose setup.
+
+mongoose.connect(url + "/" + dbName);
+
+// Schema defines the form of each document.
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified!"]
+  },
+  rating: {
+    // Validation code in mongoose.
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: String
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      review: "Great stuff!"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.insertedCount);
-    assert.equal(3, Object.keys(result.insertedIds).length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+// Model defines the collection inside the db (written singularly,
+// mongoose will parse as plural?), and the schema each document will
+// conform to.
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  });
-}
+// Document creation.
+// const fruit = new Fruit({
+//   name: "Apple",
+//   rating: 7,
+//   review: "Pretty solid as a fruit."
+// });
+// fruit.save(); // Comment out for repeated running.
+
+// Person db.
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   age: Number
+// });
+// const Person = mongoose.model("Person", personSchema);
+// const person = new Person({
+//   name: "John",
+//   age: 37
+// });
+// person.save()
+
+// const kiwi = new Fruit({
+//   name: "Kiwi",
+//   rating: 10,
+//   review: "The best fruit!"
+// });
+//
+// const orange = new Fruit({
+//   name: "Orange",
+//   rating: 4,
+//   review: "Too sour for me."
+// });
+//
+// const banana = new Fruit({
+//   name: "Banana",
+//   rating: 3,
+//   review: "Weird texture."
+// });
+
+// Model method.
+// Fruit.insertMany([kiwi, orange, banana], function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved all fruits to fruitsDB");
+//   }
+// });
+
+// Fruit.find(function(err, fruits) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//
+//     mongoose.connection.close();
+//
+//     fruits.forEach(function(fruit) {
+//       console.log(fruit.name);
+//     });
+//   }
+// });
+
+// Relationships
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
+});
+const Person = mongoose.model("Person", personSchema);
+
+// const pineapple = new Fruit({
+//   name: "Pineapple",
+//   rating: 9,
+//   review: "Great fruit."
+// });
+// pineapple.save();
+//
+// const person = new Person({
+//   name: "Amy",
+//   age: 12,
+//   favoriteFruit: pineapple
+// });
+// person.save()
+
+const mango = new Fruit({
+    name: "Mango",
+    rating: 6,
+    review: "OK fruit."
+});
+mango.save();
+
+Person.updateOne({name: "John"}, {favoriteFruit: mango}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Success");
+  }
+});
